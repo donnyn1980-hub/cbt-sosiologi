444c645ee639e6ee165056671b93e262895fcf09
donnyn1980-hub <donnyn1980@gmail.com>
2026-08-21 15:13:33 +0700

    Add files via upload

---
+++ games/questions.js
@@ -0,0 +1,78 @@
+// questions.js - Bank Soal MCMA Sosiologi (Perubahan Sosial)
+const questionBank = [
+    {
+        question: 'Berikut ini yang termasuk contoh perubahan sosial yang direncanakan (planned change) adalah...',
+        options: ['Program keluarga berencana', 'Revolusi industri', 'Pembangunan infrastruktur', 'Bencana alam'],
+        correct: [0, 2]
+    },
+    {
+        question: 'Faktor-faktor yang mempengaruhi cepat atau lambatnya perubahan sosial antara lain...',
+        options: ['Kontak dengan budaya lain', 'Sistem pendidikan yang maju', 'Sikap tradisional masyarakat', 'Keterbukaan terhadap inovasi'],
+        correct: [0, 1, 3]
+    },
+    {
+        question: 'Dampak negatif dari perubahan sosial di bidang teknologi antara lain...',
+        options: ['Kesenjangan digital', 'Cybercrime', 'Akses informasi mudah', 'Pengangguran akibat otomatisasi'],
+        correct: [0, 1, 3]
+    },
+    {
+        question: 'Contoh perubahan sosial yang bersifat regresif (menurunkan kemajuan) adalah...',
+        options: ['Kembalinya praktik perbudakan modern', 'Meningkatnya angka putus sekolah', 'Revolusi hijau di pertanian', 'Urbanisasi besar-besaran'],
+        correct: [0, 1]
+    },
+    {
+        question: 'Faktor internal yang dapat mendorong perubahan sosial dalam masyarakat adalah...',
+        options: ['Penemuan baru (inovasi)', 'Pertentangan sosial (konflik)', 'Perubahan iklim', 'Bencana alam'],
+        correct: [0, 1]
+    },
+    {
+        question: 'Teori-teori tentang perubahan sosial antara lain dikemukakan oleh...',
+        options: ['Emile Durkheim', 'Karl Marx', 'Max Weber', 'Auguste Comte'],
+        correct: [0, 1, 2]
+    },
+    {
+        question: 'Proses akulturasi budaya dapat terjadi melalui...',
+        options: ['Pernikahan campuran', 'Perdagangan internasional', 'Kolonialisasi', 'Pendidikan formal'],
+        correct: [0, 1, 2]
+    },
+    {
+        question: 'Dampak positif globalisasi terhadap perubahan sosial adalah...',
+        options: ['Meningkatnya toleransi antarbudaya', 'Akses teknologi yang merata', 'Lunturnya nilai-nilai lokal', 'Kerja sama internasional'],
+        correct: [0, 3]
+    },
+    {
+        question: 'Ciri-ciri perubahan sosial yang tidak dikehendaki (unplanned change) adalah...',
+        options: ['Terjadi secara tiba-tiba', 'Membawa dampak negatif', 'Dapat diprediksi', 'Direncanakan oleh pemerintah'],
+        correct: [0, 1]
+    },
+    {
+        question: 'Sikap masyarakat yang dapat menghambat perubahan sosial adalah...',
+        options: ['Etnosentrisme', 'Konservatisme', 'Skeptisisme', 'Radikalisme'],
+        correct: [0, 1, 2]
+    },
+    {
+        question: 'Perubahan sosial yang disebabkan oleh penemuan baru di bidang transportasi contohnya...',
+        options: ['Penemuan mesin uap', 'Kendaraan listrik', 'Pesawat terbang', 'Kereta kuda'],
+        correct: [0, 1, 2]
+    },
+    {
+        question: 'Dampak perubahan sosial di bidang ekonomi yang positif adalah...',
+        options: ['Meningkatnya produktivitas', 'Terbukanya lapangan kerja baru', 'Kesenjangan ekonomi', 'Urbanisasi'],
+        correct: [0, 1]
+    },
+    {
+        question: 'Contoh perubahan sosial yang bersifat siklus (berulang) adalah...',
+        options: ['Booming ekonomi', 'Musim kemarau', 'Pemilu', 'Tren fashion'],
+        correct: [0, 3]
+    },
+    {
+        question: 'Faktor eksternal yang memicu perubahan sosial antara lain...',
+        options: ['Bencana alam', 'Perang', 'Kebijakan pemerintah', 'Inovasi teknologi'],
+        correct: [0, 1, 2]
+    },
+    {
+        question: 'Modernisasi sebagai bentuk perubahan sosial membawa dampak...',
+        options: ['Individualisme meningkat', 'Solidaritas mekanik kuat', 'Mobilitas sosial tinggi', 'Ketergantungan pada alam'],
+        correct: [0, 2]
+    }
+];
